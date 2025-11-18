bc789fd984df7a9fe68cddc9e4bc10ea698f026d
Basit-NoemDek <hassanbasitope@gmail.com>
2025-11-18 11:18:31 +0100

    update: projects

--- src/works.js
+++ src/works.js
@@ -6,8 +6,15 @@ import BV from "./assets/BV Swap.png";
 import Harmony from "./assets/Harmony.png";
 import zerionHub from "./assets/zerionHub.png";
 import coinvista from "./assets/coinvista.png";
+import alignhr from "./assets/alignhr.png";
 
 export const projects = [
+  {
+    name: "AlignHR",
+    desc: "A fullstack HR management platform for managing employees, payroll, leaves, attendance etc",
+    img: alignhr,
+    link: "https://align-hr.vercel.app/",
+  },
   {
     name: "CoinVista",
     desc: "A all-in-one crypto advanced dashboard with real time assets tracking, market analytics, and AI powered insights in a sleek, modern interface",
